81a8b4b42f070b02c376db35508b43f825e088b6
LivHed <livia.hedin@gmail.com>
2024-10-16 14:24:41 +0200

    Troubleshoot and fix the rendering error in electron app

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+//const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      //preload: path.join(__dirname, 'preload.js'), // comment out to see if it fix the error with a blank white electron app. 
       nodeIntegration: true,
       contextIsolation: false,
     },
@@ -17,6 +17,9 @@ function createWindow() {
 
   // Uncomment this line for production builds
   // win.loadFile(path.join(__dirname, '../build/index.html'));
+
+  // enables the DevTools to check for console errors
+  win.webContents.openDevTools(); 
 }
 
 app.whenReady().then(() => {
