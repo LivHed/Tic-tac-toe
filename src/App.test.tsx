b49ef6421f887808e227a694275fb946f1700522
LivHed <livia.hedin@gmail.com>
2024-10-18 17:35:09 +0200

    Tests added WIP

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,123 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {checkForWinningCombination, checkForTie, GameBoard } from './components/GameBoard'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const firstPlayer = "X"
+const secondPlayer = "O"
+//const board = require('./components/GameBoard');
+
+test('first turn is always x', () => {
+  render(<GameBoard/>)
+ /* const nextTurn = handleNextTurn() */ // cannot find handleNextTurn()
+})
+
+test('renders tic tac toe title', () => {
+  render(<App />)
+  const title = screen.getByText(/tic tac toe/i)
+  expect(title).toBeInTheDocument()
 });
+
+test('renders squares in gameboard', () => {
+  render(<App />)
+  const squares = screen.getAllByRole('button', { name: /gameboard-square/i })
+  expect(squares).toHaveLength(9)
+});
+
+test('renders reset button', () => {
+  render(<App />)
+  const reset = screen.getByText(/reset/i)
+  expect(reset).toBeInTheDocument()
+});
+
+//-------- X wins ----------
+test('Horizontal x win returns x', () => {
+  const squares = [
+    "X","X","X",
+    "","","",
+    "","",""
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("X");
+});
+
+test('Diagonal x win returns x', () => {
+  const squares = [
+    "X","","",
+    "","X","",
+    "","","X"
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("X");
+});
+
+test('Vertical x win returns x', () => {
+  const squares = [
+    "","","X",
+    "","","X",
+    "","","X"
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("X");
+});
+
+//--------- O wins ---------
+test('Vertical o win returns o', () => {
+  const squares = [
+    "","O","",
+    "","O","",
+    "","O",""
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("O");
+});
+
+test('Diagonal o win returns o', () => {
+  const squares = [
+    "","","O",
+    "","O","",
+    "O","",""
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("O");
+});
+
+test('Horizontal o win returns o', () => {
+  const squares = [
+    "","","",
+    "","","",
+    "O","O","O"
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("O");
+});
+
+test('No winning combination returns empty string', () => {
+  const squares = [
+    "X","O","",
+    "O","X","X",
+    "O","X","O"
+  ]
+  const winner = checkForWinningCombination(squares)
+  expect(winner).toBe("");
+});
+
+test('All squares are filled and winner is an empty string, its a tie and returns true', () => {
+  const squares = [
+    "X","O","X",
+    "X","X","O",
+    "O","X","O"
+  ]
+  const winner = ""
+  const tie = checkForTie(squares, winner)
+  expect(tie).toBe(true);
+});
+
+//---------------- test scenarios to write -------------------//
+//- when a player clicks X, next turn is O
+//- when a player clicks O, next turn is X
+//- when reset button is clicked, the squares gets empty
+//- when X has three in a row, the winning x message shows
+//- when O has three in a row, the winning o message shows
+//- when neither O or X has three in a row, show Tie message
+//- when there is a win, you can't continue to click on the board.
