f08a5d98df2f55155893b3a6e3a872caee886ee6
LivHed <livia.hedin@gmail.com>
2024-10-21 10:44:29 +0200

    Clean up remove comments and console logs

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -18,7 +18,6 @@ const winningCombinations = [
 export function checkForWinningCombination(squares: any) {
   let winningPlayer = ""
     for(let i = 0; i < winningCombinations.length; i++) {
-      // first, second, third element in each array in the winningcombination array, i starts at 0. 
        const [first, second, third] = winningCombinations[i]
 
        if (squares[first] === "X" && squares[second] === "X" && squares[third] === "X") {
@@ -27,18 +26,13 @@ export function checkForWinningCombination(squares: any) {
         winningPlayer = "O"
        }
     }
-
-    console.log(winningPlayer)
-    // när alla kombinationer har loopats igenom och varken X eller O är the winner så returneras tom sträng
     return winningPlayer
 }
 
 export const checkForTie = (squares: any, winner: string) => {
-  // innehåller arrayn några tomma strängar ""
   const isBoardNotFull = squares.includes("")
-  // Om boarden är fylld med O's och X's, alltså inte innehåller några tomma strängar, och om winner är lika med tom sträng, returnera true, annars false
+
   if (!isBoardNotFull && winner === "") {
-    console.log("its a tie")
     return true
   }
   return false
@@ -52,40 +46,32 @@ export function GameBoard() {
   const [isWin, setIsWin] = useState(false)
   const [isTie, setIsTie] = useState(false)
 
-  useEffect(() => { 
-    const winner = checkForWinningCombination(squares);
-    console.log("The winner is: " + winner);
-    setWinner(winner)
+    useEffect(() => { 
+      const winner = checkForWinningCombination(squares);
+      setWinner(winner)
 
-    //  if winner is not "" (there is not a winner) set win to true (someone has won) 
-    if (winner !== "") {
-      console.log("We have a winner!")
-      setIsWin(true)
-    }
-
-    let tie = checkForTie(squares, winner)
-    setIsTie(tie)
-  }, [squares]);
-
-   const handleNextTurn = (index: number) => {
-    // if there is not a winner (!isWin), run the rest of the code in the function (and one can continue clicking on the squares)
-      if (!isWin && !squares[index]) {
-        //Visa antingen X eller O beroende på vems tur det är
-        //Om isXNext är true, visas "X", annars "O"
-        const newSquares = squares.slice()
-        newSquares[index] = isNext ? "X" : "O"
-        console.log(newSquares)
-        setSquares(newSquares)
-        ////När man klickar en ruta sätts "X" eller "O" beroende på värdet av isXNext. Efter att en ruta har klickats växlar turen genom att uppdatera isXNext med !isXNext
-        setIsNext(!isNext)
-        setPlayer(isNext ? "O" : "X") // Uppdatera vems tur det är
+      if (winner !== "") {
+        setIsWin(true)
       }
-  }
+
+      let tie = checkForTie(squares, winner)
+      setIsTie(tie)
+    }, [squares]);
+
+    const handleNextTurn = (index: number) => {
+        if (!isWin && !squares[index]) {
+          const newSquares = squares.slice()
+          newSquares[index] = isNext ? "X" : "O"
+          setSquares(newSquares)
+          setIsNext(!isNext)
+          setPlayer(isNext ? "O" : "X")
+        }
+    }
 
     const resetToEmptyBoard = () => {
       setSquares(Array(9).fill(""))
       setIsNext(true)
-      setPlayer("X") // återställ till X tur som alltid kör först
+      setPlayer("X")
       setIsWin(false)
       setIsTie(false)
       setWinner("")
